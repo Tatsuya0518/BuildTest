55380b58981281a13a458e8bcaecd4c470f53282
Tatsuya0518 <46860859+Tatsuya0518@users.noreply.github.com>
2023-12-08 01:04:58 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-hello world
+import streamlit as st
+
+def greeting1():
+    st.header('こんにちは, 世界！')
+    st.write('ねこはかわいい')
+
+def greeting2():
+    st.header('またまたこんにちは, 世界！！')
+    st.write('ねこはとてもかわいい')
+
+greeting1()
+greeting2()
