06ff1edf05808fdca36825a23eb189972f2513db
Tatsuya0518 <46860859+Tatsuya0518@users.noreply.github.com>
2023-12-09 19:18:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def greeting2():
     st.header('またまたこんにちは, 世界！！')
     st.write('ねこはとてもかわいい')
 
+def greeting3():
+    st.header('またまたまたこんにちは, 世界！！')
+    st.write('ねこはずっとかわいい')
+
 greeting1()
 greeting2()
+greeting3()
